d3fec0ab94283dac2b3838ae32de2f033e6a7d79
Nick <nicolas.cinera@gmail.com>
2023-03-15 18:45:26 -0400

    Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import './App.css'
+import Match from './pages/Match'
+import Leaderboard from './pages/Leaderboard'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <main>
-      <h1> League of Babes Refactor</h1>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<h1>404 - Not Found</h1>} />
+        <Route path="/champions" element={<h1>Champions</h1>} />
+        <Route path="/match" element={<Match />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
