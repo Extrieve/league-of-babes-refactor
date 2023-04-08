64678fb7ca4b26f849c97affb141c107cd825b13
Nick <nicolas.cinera@gmail.com>
2023-04-08 18:04:06 -0400

    Better Response

--- src/service/ChampionService.ts
+++ src/service/ChampionService.ts
@@ -33,5 +33,13 @@ export const getChampionById = async (id: string, version: string) => {
     const external_api = `https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`;
     const response = await axios.get(external_api);
     const champion = response.data.data[id];
-    return champion;
+    const payload = {
+        id: champion.key,
+        name: champion.name,
+        title: champion.title,
+        blurb: champion.blurb,
+        imageUrl: `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`,
+        tags: champion.tags
+    };
+    return payload;
 }
