8bce87ca90fea996ca74e698c199bb50867610f4
Nick <nicolas.cinera@gmail.com>
2023-04-12 23:41:05 -0400

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>
+          Made with ❤️ by Extrieve
+          <a href="//github.com/extrieve" target="_blank">
+            Nick
+          </a>
+          .
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
