c7504306030d10253ec4d27e5a830f5d1c66319a
Nick <nicolas.cinera@gmail.com>
2023-04-17 21:08:32 -0400

    Footer in Main

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import NotFound from './pages/NotFound'
 import Champions from './pages/Champions'
 import Navbar from './components/Navbar'
 import ChampionPage from './pages/ChampionPage'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </main>
   );
 }
