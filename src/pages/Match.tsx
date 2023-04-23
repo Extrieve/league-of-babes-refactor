6636abd1b60ef6bf7e1c61d66b2e499393d38faa
Nick <nicolas.cinera@gmail.com>
2023-04-22 22:21:55 -0400

    Ugh

--- src/pages/Match.tsx
+++ src/pages/Match.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import Champion from "../data/iChampion";
-import { getAllChampions } from "../service/ChampionService";
-import ChampionCard from "../components/ChampionCard";
+import React, { useEffect, useState } from 'react';
+import Champion from '../data/iChampion';
+import { getAllChampions } from '../service/ChampionService';
+import ChampionCard from '../components/ChampionCard';
+import { useNavigate } from 'react-router-dom';
 
-interface MatchSate {
+interface MatchState {
   champions: Champion[];
   champion1: Champion;
   champion2: Champion;
@@ -12,110 +13,118 @@ interface MatchSate {
   winner?: Champion;
 }
 
-class Match extends React.Component<any, MatchSate> {
+const Match: React.FC = () => {
+  const [state, setState] = useState<MatchState>({
+    champions: [],
+    champion1: {
+      id: '',
+      name: '',
+      imageUrl: '',
+      blurb: '',
+      title: '',
+      tags: [],
+    },
+    champion2: {
+      id: '',
+      name: '',
+      imageUrl: '',
+      blurb: '',
+      title: '',
+      tags: [],
+    },
+    votes1: 0,
+    votes2: 0,
+  });
 
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      champions: [],
-      champion1: {
-        id: '',
-        name: '',
-        imageUrl: '',
-        blurb: '',
-        title: "",
-        tags: [],
-      },
-      champion2: {
-        id: '',
-        name: '',
-        imageUrl: '',
-        blurb: '',
-        title: "",
-        tags: []
-      },
-      votes1: 0,
-      votes2: 0
-    };
-  }
+  const navigate = useNavigate();
 
-  shuffleInPlace(array: Champion[]) {
+  const shuffleInPlace = (array: Champion[]) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
-  }
+  };
 
-  async componentDidMount() {
-    const response = await fetch('https://ddragon.leagueoflegends.com/api/versions.json').then((response) => response.json())
-    const version = response[0];
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://ddragon.leagueoflegends.com/api/versions.json').then((response) => response.json());
+      const version = response[0];
 
-    getAllChampions(version).then((champions: Champion[]) => {
-      this.shuffleInPlace(champions);
-      this.setState({
-        champion1: champions[0],
-        champion2: champions[1],
-        champions: champions.slice(2),
+      getAllChampions(version).then((champions: Champion[]) => {
+        shuffleInPlace(champions);
+        setState({
+          champion1: champions[0],
+          champion2: champions[1],
+          champions: champions.slice(2),
+          votes1: 0,
+          votes2: 0,
+        });
       });
-    });
-  }
+    }
 
-  handleVote(champion: Champion) {
-    if (champion.id === this.state.champion1?.id) {
-      this.setState({ votes1: this.state.votes1 + 1, votes2: 0, champion2: this.state.champions[0] });
-      this.state.champions.shift();
+    fetchData();
+  }, []);
+
+  const handleVote = (champion: Champion) => {
+    if (champion.id === state.champion1.id) {
+      setState((prevState) => ({
+        ...prevState,
+        votes1: prevState.votes1 + 1,
+        votes2: 0,
+        champion2: prevState.champions[0],
+        champions: prevState.champions.slice(1),
+      }));
     } else {
-      this.setState({ votes2: this.state.votes2 + 1, votes1: 0, champion1: this.state.champions[0] });
-      this.state.champions.shift();
+      setState((prevState) => ({
+        ...prevState,
+        votes2: prevState.votes2 + 1,
+        votes1: 0,
+        champion1: prevState.champions[0],
+        champions: prevState.champions.slice(1),
+      }));
     }
 
-    if (this.state.votes1 > 4 || (this.state.votes1 > this.state.votes2 && this.state.champions.length === 0)) {
-      this.setState({ winner: this.state.champion1 });
-      alert('Winner: ' + this.state.champion1.name);
-    } else if (this.state.votes2 > 4 || (this.state.votes2 > this.state.votes1 && this.state.champions.length === 0)) {
-      this.setState({ winner: this.state.champion2 });
-      alert('Winner: ' + this.state.champion2.name);
+    if (state.votes1 > 4 || (state.votes1 > state.votes2 && state.champions.length === 0)) {
+      setState((prevState) => ({ ...prevState, winner: prevState.champion1 }));
+      navigate(`/champions/${state.champion1.name}`);
+    } else if (state.votes2 > 4 || (state.votes2 > state.votes1 && state.champions.length === 0)) {
+      setState((prevState) => ({ ...prevState, winner: prevState.champion2 }));
+      navigate(`/champions/${state.champion2.name}`);
     }
-      
-  }
+  };
 
-  render() {
-    console.log(this.state.champion2);
-    return (
-      <div>
-        <div className="row">
-          <div className="col">
-            <ChampionCard
-              name={this.state.champion1?.name || ''}
-              title={this.state.champion1?.title || ''}
-              image={this.state.champion1?.imageUrl || ''}
-              id={this.state.champion1?.id || ''}
-              blurb={this.state.champion1?.blurb || ''}
-              cardSize={56}
-            />
-            <div>
-              <button onClick={() => this.handleVote(this.state.champion1)}>Vote</button>
-              <p>Votes: {this.state.votes1}</p>
-            </div>
-          </div>
-          <div className="col">
-            <ChampionCard
-              name={this.state.champion2?.name || ''}
-              title={this.state.champion2?.title || ''}
-              image={this.state.champion2?.imageUrl || ''}
-              id={this.state.champion2?.id || ''}
-              blurb={this.state.champion2?.blurb || ''}
-              cardSize={56}
-            />
-            <div>
-              <button onClick={() => this.handleVote(this.state.champion2)}>Vote</button>
-              <p>Votes: {this.state.votes2}</p>
-            </div>
-          </div>
+  return (
+    <div>
+      <div className="row">
+        <div className="col">
+          <ChampionCard
+            name={state.champion1.name || ''}
+            title={state.champion1.title || ''}
+            image={state.champion1.imageUrl || ''}
+            id={state.champion1.id || ''}
+            blurb={state.champion1.blurb || ''}
+            cardSize={24}
+          />
+          <button className="btn btn-primary" onClick={() => handleVote(state.champion1)}>
+            Vote
+          </button>
+        </div>
+        <div className="col">
+          <ChampionCard
+            name={state.champion2.name || ''}
+            title={state.champion2.title || ''}
+            image={state.champion2.imageUrl || ''}
+            id={state.champion2.id || ''}
+            blurb={state.champion2.blurb || ''}
+            cardSize={24}
+          />
+          <button className="btn btn-primary" onClick={() => handleVote(state.champion2)}>
+            Vote
+          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Match;
