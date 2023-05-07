3ba73c022dfd29a7933f62a1ad495e0dccf83312
Nick <nicolas.cinera@gmail.com>
2023-05-06 20:56:46 -0400

    Convert Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,37 @@
-import React from "react";
 import Champion from "../data/iChampion";
 
-
-class Navbar extends React.Component {
-
-  constructor(props: any) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">League of Legends</a>
-          <ul className="nav justify-content-end">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/champions">Champions</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/match">Match</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/leaderboard">Leaderboard</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+function Navbar(props: any) {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          League of Legends
+        </a>
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/champions">
+              Champions
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/match">
+              Match
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/leaderboard">
+              Leaderboard
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
