c1af726a6adda176dea7e29e26a65d74b4e00748
Nick <nicolas.cinera@gmail.com>
2023-05-17 21:48:58 -0400

    Home refactor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,26 @@
-import React from 'react'
-
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="home">
-        <h1>
-          League of Babes
-        </h1>
-        <p className="home-description">
-          Find out who's your favorite Babe across Summoner's Rift 😳
-        </p>
-        <div className='go'>
-          <a href="/match">
-            <button className="home-button">
-              Find your Babe
-            </button>  
-          </a>
-          <a href="/champions">
-            <button className="home-button">
-              Explore Babes
-            </button>
-          </a>
-        </div>
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>
+        League of Babes
+      </h1>
+      <p className="home-description">
+        Find out who's your favorite Babe across Summoner's Rift 😳
+      </p>
+      <div className='go'>
+        <a href="/match">
+          <button className="home-button">
+            Find your Babe
+          </button>
+        </a>
+        <a href="/champions">
+          <button className="home-button">
+            Explore Babes
+          </button>
+        </a>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Home;
